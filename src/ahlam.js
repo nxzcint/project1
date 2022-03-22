a4bdb4843c8a0383a9c86b725566ad57f335740c
AhlamAA <98388927+AhlamAA@users.noreply.github.com>
2022-03-22 10:19:39 +0300

    The Correct Solution

--- src/ahlam.js
+++ src/ahlam.js
@@ -7,4 +7,4 @@ function Ahlam() {
     );
   }
   
-  export default App;
+  export default Ahlam;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Ahlam from './Ahlam'; 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
